e98bb68681bd23dc9717f84a06a6e21ef84fd47f
Llewellyn Falco <isidore@setgame.com>
2017-05-04 08:55:14 -0500

    added reciepe

--- SimpleSquare.py
+++ SimpleSquare.py
@@ -12,3 +12,10 @@ for i in range(sides):
     Turtle.turn(360.0 / sides)
     Turtle.show()
 
+# Show the tortoise --#1
+# Make the tortoise move as fast as possible --#6
+# Do the following 4 times --#5.1
+#       Change the color of the line the tortoise draws to "blue" --#4
+#       Move the tortoise 50 pixels --#2
+#       Turn the tortoise to the right (90 degrees) --#3
+# Repeat --#5.2
